4de29735f5d53fede0cd78dc8e821ad519b187e7
Rohit Khandelwal <rohit.khandelwal.2059@gmail.com>
2026-05-12 23:22:53 +0530

    fixed docker host binding

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Pipeline Working!");
+  res.send("CI/CD Pipeline Working!");
 });
 
 app.get("/health", (req, res) => {
-    res.status(200).json({
-        status: "UP"
-    });
+  res.json({ status: "UP" });
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
